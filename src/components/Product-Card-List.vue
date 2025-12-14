61738d5bb6428df2aad39ab0fb397c8554b34ee0
MuaazHanif1 <muaazhanif2@gmail.com>
2025-12-14 15:11:30 +0500

    'get'

--- src/components/Product-Card-List.vue
+++ src/components/Product-Card-List.vue
@@ -3,7 +3,7 @@
     <section class="products">
          <h2>Featured Products</h2>
          <div class="product-list flex-center">
-            <ProductCard v-for="value in store.products"  :id="value.id" :name="value.name" :imgSrc="value.imgPath" :price="value.price"></ProductCard>    
+            <ProductCard v-for="value in store.products"  :id="value.id" :name="value.title" :imgSrc="value.image" :price="value.price"></ProductCard>    
          </div>
     </section>
 </template>
@@ -12,5 +12,6 @@ import ProductCard from './Product-Card.vue';
 import {useProductList} from '../store/productlist'
 
 const store = useProductList()
+store.getProductListing()
 
 </script>
